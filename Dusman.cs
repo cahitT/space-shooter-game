eede62a5d3893cd587ab554d948f0f1c733953d8
Cahit Tulum <cahittulum@gmail.com>
2018-02-21 17:11:03 +0300

    Enemy's properties(size, location, coordinate, picture etc.)

---
+++ Dusman.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace cemal
+{
+    class Dusman:PictureBox
+    {
+        PictureBox dusman;
+        public List<PictureBox> dusmanlar=new List<PictureBox>();
+        public int dusmanIndex = 0;
+        public int index=0;
+
+
+        public void dusmanGemisiOlustur(int x, int y,MainPencere ciz)
+        {
+            dusman = new PictureBox();
+            dusman.ImageLocation = "enemyShip.png";
+            dusman.SizeMode = PictureBoxSizeMode.StretchImage;
+            dusman.Size = new Size(50, 45);
+            dusman.Location = new Point(x, y);
+            dusman.BackColor = Color.Transparent;
+            dusmanlar.Add(dusman);
+            ciz.Controls.Add(dusmanlar[dusmanIndex]);
+            dusmanIndex++;
+        }
+
+        public void hareket()
+        {
+            for(int i=0;i<dusmanlar.Count;i++)
+            {
+                dusmanlar[i].Top += 7;
+            }
+        }
+
+        public int dYukseklikGetir()
+        {
+            return dusman.Height;
+        }
+    }
+}
