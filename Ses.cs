f30c582be3dfe6dff54b980819190a4eb92d4e24
Cahit Tulum <cahittulum@gmail.com>
2018-02-21 17:20:32 +0300

    The class that gives sound when we fire

---
+++ Ses.cs
@@ -0,0 +1,20 @@
+﻿using System.Media;
+
+namespace cemal
+{
+    class Ses
+    {
+        SoundPlayer player = new SoundPlayer();
+
+        public void mermiSesi()
+        {   
+            player.SoundLocation = "gun.wav";
+            player.Play();
+        }
+        public void bitisSesi()
+        {
+            player.SoundLocation = "sidious.wav";
+            player.Play();
+        }
+    }
+}
